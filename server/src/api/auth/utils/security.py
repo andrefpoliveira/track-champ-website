7eac1452ec6622768bb59c924f044ad00a1743fb
andre.oliveira <andre.oliveira@mollie.com>
2024-10-10 16:29:18 +0100

    Creation of a verification code

--- server/src/api/auth/utils/security.py
+++ server/src/api/auth/utils/security.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 
 import bcrypt
+import secrets
 
 def encrypt_password(password: str, salt: str = None) -> Tuple[str]:
 	"""
@@ -20,3 +21,16 @@ def encrypt_password(password: str, salt: str = None) -> Tuple[str]:
 		salt = bytes(salt, 'utf-8')
 		
 	return bytes.decode(bcrypt.hashpw(bytes(password, 'utf-8'), salt)), bytes.decode(salt)
+
+
+def generate_verification_code(length: int) -> str:
+	"""
+	Generates a cryptographically secure x-digit verification code.
+
+	Parameters:
+		length (int): The length of the verification code
+
+	Returns:
+		(str): The verification code with size length
+	"""
+	return ''.join(str(secrets.choice(range(10))) for i in range(length))
