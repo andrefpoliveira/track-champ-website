ceac77c622081f07240d3aac7b7e636d3f32be91
andre.oliveira <andre.oliveira@mollie.com>
2024-10-21 11:30:46 +0100

    Sidebar animations

--- website/src/Components/Sidebar/Sidebar.js
+++ website/src/Components/Sidebar/Sidebar.js
@@ -3,24 +3,48 @@ import React from "react";
 
 import { FaUsers } from "react-icons/fa";
 import { FaChartLine } from "react-icons/fa";
-import { FaChevronLeft } from "react-icons/fa";
+import { BsChevronBarLeft } from "react-icons/bs";
+import { BsChevronBarRight } from "react-icons/bs";
 
-export default function Sidebar() {
-	return (
-		<div id="sidebar">
-			<div id="resize-button">
-				<FaChevronLeft />
-			</div>
+export default class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sidebarOpen: true
+		};
+	}
 
-			<div class="sidebar-item">
-				<FaUsers />
-				<span>Equipas</span>
-			</div>
+	toggleOpen() {
+		this.setState({ sidebarOpen: !this.state.sidebarOpen })
+	}
 
-			<div class="sidebar-item">
-				<FaChartLine />
-				<span>Resultados</span>
+	render() {
+		return (
+			<div id="sidebar" className={this.state.sidebarOpen ? 'opened' : 'closed'}>
+				<div
+					class="sidebar-item toggle-button"
+					onClick={() => this.toggleOpen()}
+				>
+					{
+						this.state.sidebarOpen
+						? <BsChevronBarLeft/>
+						: <BsChevronBarRight />
+					}
+					
+				</div>
+
+				<hr />
+	
+				<div class="sidebar-item">
+					<FaUsers />
+					<span>Equipas</span>
+				</div>
+	
+				<div class="sidebar-item">
+					<FaChartLine />
+					<span>Resultados</span>
+				</div>
 			</div>
-		</div>
-	);
+		)
+	}
 }
