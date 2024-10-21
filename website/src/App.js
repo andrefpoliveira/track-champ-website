589a3d744ddbde85775bcdd740f79f0f5c0daee6
andre.oliveira <andre.oliveira@mollie.com>
2024-10-21 09:11:51 +0100

    Sidebar and resize button structure

--- website/src/App.js
+++ website/src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 
+import React from 'react';
+
 import { Outlet } from 'react-router-dom';
 
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
+
+import AuthContext from './Logic/AppContext';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 
-			<div id="content">
-				<Sidebar />
-				<Outlet />
-			</div>
+			<AuthContext.Consumer>
+				
+				{({ profile, setProfile }) => (
+					<>
+						{
+							profile
+								? <div id="content">
+									<Sidebar />
+									<Outlet />
+								</div>
+								: <div>Some Information Here</div>
+						}
+					</>
+				)}
+			</AuthContext.Consumer>
 		</div>
 	);
 }
--- website/src/Components/Sidebar/Sidebar.js
+++ website/src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,26 @@
 import './Sidebar.css';
 import React from "react";
 
+import { FaUsers } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
+
 export default function Sidebar() {
 	return (
 		<div id="sidebar">
+			<div id="resize-button">
+				<FaChevronLeft />
+			</div>
 
+			<div class="sidebar-item">
+				<FaUsers />
+				<span>Equipas</span>
+			</div>
+
+			<div class="sidebar-item">
+				<FaChartLine />
+				<span>Resultados</span>
+			</div>
 		</div>
 	);
 }
